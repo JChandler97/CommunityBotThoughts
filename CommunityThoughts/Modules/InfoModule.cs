8fb4c9258116b99c8a210ea887d4179c4b49e017
James <jameschandlerjr@hotmail.com>
2024-07-08 02:07:17 -0400

    Add project files.

---
+++ CommunityThoughts/Modules/InfoModule.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Tweetinvi;
+using Tweetinvi.Client;
+using CommunityThoughts;
+using Discord;
+using Tweetinvi.Parameters;
+using System.Linq;
+
+namespace CommunityThoughts.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("tweet")]
+        [Summary("I sing your muse to Twitter.")]
+        public async Task TweetAsync([Remainder] [Summary("I sing your muse to Twitter")] string echo)
+        {
+            var userInfo = Context.User;
+            string letterCheck = $"\"{echo}\" -{userInfo.Username}";
+            if (letterCheck.Length <= 280)
+            {
+                
+                var userClient = new TwitterClient(botToken.consumerKey, botToken.consumerSecret, botToken.accessToken, botToken.accessTokenSecret);
+                var tweet = await userClient.Tweets.PublishTweetAsync($"\"{echo}\" -{userInfo.Username}");
+                await ReplyAsync($"Tweet sent. :slight_smile:");
+            }
+            else
+            {
+                await ReplyAsync("Tweet has not been sent because it's too long. Please shorten your message or name. :slight_frown:");
+            }
+
+        }
+
+        /*
+        [Command("ptweet")]
+        [Summary("Sends a tweet with a picture attached.")]
+        public async Task PictureTweetAsync([Summary("Sends a tweet with a picture attached.")] string echo)
+        {
+            var userInfo = Context.User;
+            var picture = Context.Message.Attachments;
+            string letterCheck = $"\"{echo}\" -{userInfo.Username}#{userInfo.Discriminator}";
+            if (letterCheck.Length <= 280)
+            {
+                var userClient = new TwitterClient(botToken.consumerKey, botToken.consumerSecret, botToken.accessToken, botToken.accessTokenSecret);
+                
+                //$"\"{echo}\" -{userInfo.Username}#{userInfo.Discriminator}"
+                var tweetWithImage = await userClient.Tweets.PublishTweetAsync(new PublishTweetParameters($"\"{echo}\" -{userInfo.Username}#{userInfo.Discriminator}")
+                {
+                    Medias = { picture }
+                });
+                await ReplyAsync($"Tweet sent. :slight_smile:");
+            }
+            else
+            {
+                await ReplyAsync("Tweet has not been sent because it's too long. Please shorten your message or name. :slight_frown:");
+            }
+        }  */
+
+        [Command("check")]
+        [Summary("Echoes a message.")]
+        public async Task CheckAsync([Summary("The text to echo")] int num = 0) // the fact you HAVE to use a variable for this to work is tacky
+        {
+            //var userInfo
+            var userInfo = Context.User;
+
+            //IReadOnlyCollection<SocketMessage> GetCachedMessages(IMessage message, Direction Before, int limit = 1);
+            await ReplyAsync($"I am online! 🙂");
+
+        }
+
+    /*
+
+    I NEED THIS
+    await ReplyAsync($"{echo} by {userInfo.Username}#{userInfo.Discriminator}");
+    */
+
+        [Command("echo")]
+        [Summary("Echoes a message.")]
+        public Task EchoAsync([Remainder][Summary("I sing your muse to Twitter")] string echo) 
+            => ReplyAsync(echo);
+
+    }
+
+
+    /*
+    [Group("sample")]
+    public class SampleModule : ModuleBase<SocketCommandContext>
+    {
+        // ~sample square 20 -> 400
+        [Command("square")]
+        [Summary("Squares a number.")]
+        public async Task SquareAsync(
+            [Summary("The number to square.")]
+        int num)
+        {
+            // We can also access the channel from the Command Context.
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+
+        // ~sample userinfo --> foxbot#0282
+        // ~sample userinfo @Khionu --> Khionu#8708
+        // ~sample userinfo Khionu#8708 --> Khionu#8708
+        // ~sample userinfo Khionu --> Khionu#8708
+        // ~sample userinfo 96642168176807936 --> Khionu#8708
+        // ~sample whois 96642168176807936 --> Khionu#8708
+        [Command("userinfo")]
+        [Summary
+        ("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(
+            [Summary("The (optional) user to get info from")]
+        SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    } */
+}
---
+++ CommunityThoughts/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityThoughts;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Tweetinvi;
+
+public class Program
+{
+    // Program entry point
+    static void Main(string[] args)
+    {
+        // Call the Program constructor, followed by the 
+        // MainAsync method and wait until it finishes (which should be never).
+        new Program().MainAsync().GetAwaiter().GetResult();
+    }
+
+    private readonly DiscordSocketClient _client;
+
+    // Keep the CommandService and DI container around for use with commands.
+    // These two types require you install the Discord.Net.Commands package.
+    private readonly CommandService _commands;
+    private readonly IServiceProvider _services;
+
+    private Program()
+    {
+        var config = new DiscordSocketConfig()
+        {
+            // How much logging do you want to see?
+            LogLevel = LogSeverity.Info,
+
+            GatewayIntents = GatewayIntents.All,
+
+            // If you or another service needs to do anything with messages
+            // (eg. checking Reactions, checking the content of edited/deleted messages),
+            // you must set the MessageCacheSize. You may adjust the number as needed.
+            MessageCacheSize = 5,
+
+            // If your platform doesn't have native WebSockets,
+            // add Discord.Net.Providers.WS4Net from NuGet,
+            // add the `using` at the top, and uncomment this line:
+            //WebSocketProvider = WS4NetProvider.Instance
+        };
+
+        _commands = new CommandService(new CommandServiceConfig
+        {
+            // Again, log level:
+            LogLevel = LogSeverity.Info,
+
+            // There's a few more properties you can set,
+            // for example, case-insensitive commands.
+            CaseSensitiveCommands = false,
+        });
+
+
+        _client = new DiscordSocketClient(config);
+
+        // Subscribe the logging handler to both the client and the CommandService.
+        _client.Log += Log;
+        _commands.Log += Log;
+
+        // Setup your DI container.
+        //_services = ConfigureServices();
+
+    }
+
+    // Example of a logging handler. This can be re-used by addons
+    // that ask for a Func<LogMessage, Task>.
+    private static Task Log(LogMessage message)
+    {
+        switch (message.Severity)
+        {
+            case LogSeverity.Critical:
+            case LogSeverity.Error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case LogSeverity.Warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogSeverity.Info:
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case LogSeverity.Verbose:
+            case LogSeverity.Debug:
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                break;
+        }
+        Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+        Console.ResetColor();
+        return Task.CompletedTask;
+    }
+
+    private async Task MainAsync()
+    {
+        // Centralize the logic for commands into a separate method.
+        await InitCommands();
+
+        // Login and connect.
+        await _client.LoginAsync(TokenType.Bot, botToken.clientToken);
+        await _client.StartAsync();
+
+        // Wait infinitely so your bot actually stays connected.
+        await Task.Delay(-1);
+    }
+
+    private async Task InitCommands()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+        // Don't process the command if it was a system message
+        var message = messageParam as SocketUserMessage;
+        if (message == null) return;
+
+        // Create a number to track where the prefix ends and the command begins
+        int argPos = 0;
+
+        // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+        if (!(message.HasCharPrefix('!', ref argPos) ||
+            message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+            message.Author.IsBot)
+            return;
+
+        // Create a WebSocket-based command context based on the message
+        var context = new SocketCommandContext(_client, message);
+
+        // Execute the command with the command context we just
+        // created, along with the service provider for precondition checks.
+        await _commands.ExecuteAsync(
+            context: context,
+            argPos: argPos,
+            services: null);
+    }
+}
